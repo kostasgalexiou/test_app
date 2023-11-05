0bff72e9be0be69ed5dbc8a4520dc4e484e54a95
kostasgalexiou <konstantinos.alexiou@irta.cat>
2023-11-05 16:22:17 +0100

    update

---
+++ main.py
@@ -0,0 +1,30 @@
+# streamlit_app.py
+
+import streamlit as st
+from supabase import create_client, Client
+
+print(st.secrets)
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
+
+
+supabase = init_connection()
+
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query():
+    return supabase.table("mytable").select("*").execute()
+
+
+rows = run_query()
+
+# Print results.
+for row in rows.data:
+    st.write(f"{row['name']} has a :{row['pet']}:")
